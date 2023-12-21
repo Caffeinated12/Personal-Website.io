9729e44f3a4999e5fb039789a8154a3a6bd88fde
Caffeinated12 <Charlesditching@gmail.com>
2023-12-22 04:29:31 +0800

    added small mobile support

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 var LocalTheme = localStorage.getItem("theme");
+
 var systemDark = window.matchMedia("(prefers-color-scheme: dark)");
+
 var htmlELement = document.documentElement.getAttribute("data-theme");
+
 var button = document.getElementById("Theme_Mode");
+
 var initial_theme = LocalTheme || (systemDark.matches ? "dark" : "light");
+
 document.documentElement.setAttribute("data-theme", initial_theme);
+
 button.checked = initial_theme === "dark";
+
 function changetheme(){
     currenttheme = document.documentElement.getAttribute("data-theme");
     newtheme = currenttheme === 'dark' ? 'light' : "dark";
@@ -14,3 +21,22 @@ function changetheme(){
 }
 button.addEventListener("click", changetheme);
 
+var hamburgerbtn = document.getElementById("NavShow")
+var navbar = document.getElementById("NavSelect")
+var navitems = document.getElementsByClassName('NavItems')
+
+function showNavSelect(){
+    if((hamburgerbtn.checked === true) || (navbar.className === "NavStatus")){
+        navbar.className += " Toggled"
+        for(let i = 0; i < 3; i++){
+            navitems[i].className += " Show"
+        }
+    }else {
+        navbar.className = "NavStatus"
+        for(let i = 0; i < 3; i++){
+            navitems[i].className = "NavItems"
+        }
+    }
+}
+
+hamburgerbtn.addEventListener("click", showNavSelect)
